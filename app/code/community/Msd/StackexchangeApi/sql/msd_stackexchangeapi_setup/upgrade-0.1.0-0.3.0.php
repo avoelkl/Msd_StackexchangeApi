838e197df4ae2bc9e24dabee5d8a479126ec00cd
Anna Voelkl <anna@vape.net>
2015-02-05 21:57:14 +0100

    Added history (user log) and global site statistics

---
+++ app/code/community/Msd/StackexchangeApi/sql/msd_stackexchangeapi_setup/upgrade-0.1.0-0.3.0.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * StackExchange user data history
+ * StackExchange site info
+ * API: https://api.stackexchange.com/docs/types/user
+ *
+ * @category    Msd_StackexchangeApi
+ * @package     msd_stackexchangeapi_setup
+ * @author      Anna Völkl / @rescueAnn
+ */
+
+$installer = $this;
+$installer->startSetup();
+
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('msd_stackexchangeapi/user_history'))
+    ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity'  => true,
+        'unsigned'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ), 'Id')
+    ->addColumn('se_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Stackexchange ID from stackexchange_user table')
+    ->addColumn('user_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'User ID')
+    ->addColumn('reputation', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Reputation')
+    ->addColumn('reputation_change_day', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Reputation Change Day')
+    ->addColumn('reputation_change_week', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Reputation Change Week')
+    ->addColumn('reputation_change_month', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Reputation Change Month')
+    ->addColumn('reputation_change_year', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Reputation Change Year')
+    ->addColumn('up_vote_count', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Upvote count')
+    ->addColumn('down_vote_count', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Downvote count')
+    ->addColumn('answer_count', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Answer count')
+    ->addColumn('view_count', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'View count')
+    ->addColumn('question_count', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Question count')
+    ->addColumn('badge_counts',  Varien_Db_Ddl_Table::TYPE_TEXT, 100, array(
+    ), 'Badge counts')
+    ->addForeignKey($installer->getFkName('msd_stackexchangeapi/user_history', 'se_id', 'msd_stackexchangeapi/user', 'id'),
+        'se_id', $installer->getTable('msd_stackexchangeapi/user'), 'id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
+    ->setComment('StackExchange Type user history (log data)');
+$installer->getConnection()->createTable($table);
+
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('msd_stackexchangeapi/statistics'))
+    ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity'  => true,
+        'unsigned'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ), 'Id')
+    ->addColumn('date', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Date')
+    ->addColumn('new_active_users', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'New active users')
+    ->addColumn('total_users', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Total users')
+    ->addColumn('badges_per_minute', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Badges per minute')
+    ->addColumn('total_badges', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Total badges')
+    ->addColumn('total_votes', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Total votes')
+    ->addColumn('total_comments', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Total comments')
+    ->addColumn('answers_per_minute', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Answers per min')
+    ->addColumn('questions_per_minute', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Questions per minute')
+    ->addColumn('total_answers', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Total answers')
+    ->addColumn('total_accepted', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Total accepted')
+    ->addColumn('total_unanswered', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Total unanswered')
+    ->addColumn('total_questions', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Total questions')
+    ->addColumn('active', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Entry active')
+    ->setComment('StackExchange global site statistics data');
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
